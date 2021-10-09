e507317c1bf2dfb5081baa7548c1027b478ca4e3
Jrodriguez83 <judriguez83@gmail.com>
2021-10-09 17:47:22 -0400

    Implementar la carga de los posts

--- user_manager/lib/Screens/details_screen.dart
+++ user_manager/lib/Screens/details_screen.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_manager/Models/users_model.dart';
+import 'package:user_manager/cubit/posts_cubit.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({Key? key, required this.user}) : super(key: key);
   final User user;
   @override
   Widget build(BuildContext context) {
+    BlocProvider.of<PostsCubit>(context).getUsersPosts(user.id);
     return Scaffold(
       appBar: AppBar(
         title: Text(user.username),
       ),
-      body: Center(
-        child: Text('Body'),
+      body: BlocBuilder<PostsCubit, PostsState>(
+        builder: (context, state) {
+          if (state is! LoadedPosts) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final posts = state.posts;
+          return Container(
+            child: Column(
+              children: [
+                Text(user.name),
+                Text(user.email),
+                Text(user.phoneNumber),
+                Text(user.website),
+                ...posts.map((post) => Text(post.title))
+              ],
+            ),
+          );
+        },
       ),
     );
   }
