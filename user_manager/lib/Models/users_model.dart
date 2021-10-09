c691190a11ced5467e8e0335d3b5aad93caf567e
Jrodriguez83 <judriguez83@gmail.com>
2021-10-09 16:02:13 -0400

    Crear las clases de usuario

---
+++ user_manager/lib/Models/users_model.dart
@@ -0,0 +1,43 @@
+abstract class UserData {}
+
+class User extends UserData {
+  int id;
+  String name;
+  String username;
+  String email;
+  Address address;
+  String phoneNumber;
+  String website;
+  String companyName;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.address,
+    required this.phoneNumber,
+    required this.website,
+    required this.companyName,
+  });
+}
+
+class Address extends UserData {
+  String street;
+  String suite;
+  String city;
+  String zipcode;
+
+  Address({
+    required this.street,
+    required this.suite,
+    required this.city,
+    required this.zipcode,
+  });
+}
+
+// class Company extends UserData {
+//   String name;
+//   String catchPhrase;
+//   String bs;
+// }
