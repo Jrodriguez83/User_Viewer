1fc5f989c230546a7148ca84952d19430730a407
Jrodriguez83 <judriguez83@gmail.com>
2021-10-09 16:05:16 -0400

    Crear clase de Post

---
+++ user_manager/lib/Models/post_model.dart
@@ -0,0 +1,13 @@
+class Post {
+  int id;
+  int userId;
+  String title;
+  String body;
+
+  Post({
+    required this.id,
+    required this.userId,
+    required this.title,
+    required this.body,
+  });
+}
